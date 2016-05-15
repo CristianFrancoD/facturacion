5d17688098aa20fbb098e61b76178bf7f5b64ac6
Cristian <CristianFranco@MBP-de-Cristian.lan>
2016-05-15 15:38:02 -0600

    Se añade la funcionalidad para registrar usuarios y facturas ademas de las sesiones de usuarios

--- app.js
+++ app.js
@@ -1,33 +1,117 @@
 var express = require("express");
 var app = express();
-
+var Usuario = require("./models/cosas").Usuario;
+var Factura = require("./models/cosas").Factura;
+var bodyParser = require("body-parser");
+var session = require("express-session");
 app.set("view engine","jade");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+secret: "123kmmlkmwldmk288ad8dhu",
+resave: false,
+saveUninitialized:false
 
+}));
+
+//Muestra la vista principal que manda a registrarse o iniciar sesion
 app.get("/",function(req, res){
 
 
 
-res.render("index");
-
-
-
+  res.render("index");
+//  console.log(req.session.user_id);
 });
 
+
+//Renderiza la vista para iniciar sesion
 app.get("/iniciar", function(req,res){
 
 
-res.render("iniciar");
+  res.render("iniciar");
 
 });
 
 
+//Renderiza la vista para registrarse
 app.get("/registrar", function(req,res){
 
-res.render("registrar");
+  res.render("registrar");
+
+});
+
+
+/*
+Funcion que toma los datos introducidos en los inputs de la
+vista de registrarse y los guarda en la base de datos
+*/
+app.post("/registrar", function(req, res){
+
+  var usuario = new Usuario({
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+    email: req.body.email,
+    contrasena: req.body.contra
+
+
+  });
+
+  usuario.save().then(function(us){
+  res.redirect("/")
+  console.log("Se guardaron sus datos correctamente");
+
+  },function(err){
+
+    console.log(String(err));
+    console.log("Hubo un error al guarda el usuario")
+    res.redirect("/signup");
+  });
+
+
+
+
+});
+
+app.get("/crearfac",function(req,res){
+
+  res.render("crearfac");
+
+});
+
+app.post("/crearfac", function(req,res){
+
+  var factura = new Factura({
+    cliente: req.body.cliente,
+    RFC: req.body.rfc,
+    direccion: req.body.direccion,
+    fecha: req.body.fecha
+
+  });
+  factura.save().then(function(us){
+  res.redirect("/sessions")
+  console.log("Se guardaron sus datos correctamente");
+
+  },function(err){
+
+    console.log(String(err));
+    console.log("Hubo un error al guarda el usuario")
+    res.redirect("/crearfac");
+  });
+
 
 });
 
 
 
-app.listen(8080);
+app.post("/sessions",function(req,res){
+  Usuario.findOne({email:req.body.email, contrasena:req.body.contra},function(err,user){
+    console.log("Hola");
+  req.session.user_id = user._id;
+  res.render("dashboard");
+  console.log(req.session.user_id);
+  })
+
+});
+
+app.listen(8000);
---
+++ models/cosas.js
@@ -0,0 +1,41 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/cosas");
+var Schema = mongoose.Schema,
+ObjectId = Schema.ObjectId;
+
+var usuarioSchema = new Schema();
+var facturaSchema = new Schema();
+
+usuarioSchema.add({
+id:ObjectId,
+nombre:String,
+apellido: String,
+email:String,
+contrasena:String,
+facturas:{type : mongoose.Schema.ObjectId, ref : 'Factura'}
+});
+
+facturaSchema.add({
+cliente: String,
+RFC: String,
+direccion: String,
+fecha: String,
+noFactura: Number,
+productos: [{
+  cantidad: Number,
+  descripcion: String,
+  preciounitario: Number,
+  preciototal: Number
+}],
+  subtotal: Number,
+  iva: Number,
+  total: Number
+
+});
+
+
+module.exports = {
+
+Usuario: mongoose.model('Usuario', usuarioSchema),
+Factura: mongoose.model('Factura', facturaSchema)
+};
